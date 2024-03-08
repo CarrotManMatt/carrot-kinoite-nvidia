ddb182a0793db0e107d750a6d53885d56cef222e
Matt Norton <matt@carrotmanmatt.com>
2024-03-08 14:52:39 +0000

    Update and rename download-pam-pwdfile.sh to install-package-pam-pwdfile.sh

--- config/scripts/download-pam-pwdfile.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-set -oue pipefail
-
-wget ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/home:/Akoellh/openSUSE_Leap_42.3_Update/x86_64/pam_pwdfile-1.0-3.3.x86_64.rpm
