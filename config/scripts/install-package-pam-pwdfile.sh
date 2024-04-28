9abcf3959c5e1fbc505343f3b5f3807f3bc5fcbe
Matt Norton <matt@carrotmanmatt.com>
2024-04-28 01:29:23 +0100

    Update install-package-pam-pwdfile.sh

--- config/scripts/install-package-pam-pwdfile.sh
+++ config/scripts/install-package-pam-pwdfile.sh
@@ -2,6 +2,5 @@
 
 set -euo pipefail
 
-mkdir /tmp
 wget 'ftp://ftp.pbone.net/mirror/ftp5.gwdg.de/pub/opensuse/repositories/home:/Akoellh/openSUSE_Leap_42.3_Update/x86_64/pam_pwdfile-1.0-3.3.x86_64.rpm' -O /tmp/pam_pwdfile-1.0-3.3.x86_64.rpm
 rpm-ostree install /tmp/pam_pwdfile-1.0-3.3.x86_64.rpm
