a7a74a5d826dfba867eebfa5d6eaec16e6e38583
Matt Norton <matt@carrotmanmatt.com>
2024-07-09 22:05:17 +0100

    Update disable-secondary-screen-in-sddm.sh

--- config/scripts/disable-secondary-screen-in-sddm.sh
+++ config/scripts/disable-secondary-screen-in-sddm.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 # Inspiration for this script came from https://www.reddit.com/r/archlinux/comments/143b6we/comment/jn8zya3
 
 echo 'xrandr --output HDMI-A-1 --off' >> /usr/share/sddm/scripts/Xsetup
-echo 'xrandr --output DP-1 --off' >> /usr/share/sddm/scripts/Xsetup
 echo 'xrandr --output DP-2 --off' >> /usr/share/sddm/scripts/Xsetup
+echo 'xrandr --output DP-3 --off' >> /usr/share/sddm/scripts/Xsetup
