6688139b93405a3468c7a3c7f883a23375275f54
Matt Norton <matt@carrotmanmatt.com>
2025-07-02 12:14:09 +0100

    Delete files/scripts/bootc-update-bootloader.sh

--- files/scripts/bootc-update-bootloader.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-# This script comes from fiftydinar's Gidro-OS (https://github.com/fiftydinar/gidro-os/blob/main/config/scripts/bootc-update-bootloader.sh)
-
-# Update bootloader metadata inside the image, which will be utilized further by bootc in ISO to update the bootloader on system
-bootupctl backend generate-update-metadata
